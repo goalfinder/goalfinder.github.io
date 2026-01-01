2c028540b13f873290aa3e2764c9788c31938d57
Markus Harnúšek <m.harnusek@students.htl-leonding.ac.at>
2026-01-01 17:44:29 +0100

    Finished implementation on #25

--- web/scripts/home.js
+++ web/scripts/home.js
@@ -2,8 +2,26 @@ import { loadLang } from './i18n.js';
 
 /** Default page language */
 const defaultLang = "de";
+let langButton;
+let currentLang;
+
+function switchLang() {
+    currentLang = currentLang === "de" ? "en" : "de";
+    loadLang(currentLang)
+    
+    if (langButton) {
+        langButton.innerHTML = currentLang.toUpperCase();
+    }
+}
 
 document.addEventListener("DOMContentLoaded", () => {
     // Load the default language
-    loadLang(defaultLang);
+    currentLang = defaultLang;
+    loadLang(currentLang);
+
+    // Get language switch button
+    langButton = document.getElementById("lang-button");
+    if (langButton) {
+        langButton.onclick = function() {switchLang()};
+    }
 }); 
